528dbeaa77a1d2879bd15842084828273122b7c0
Caio Torres <caiocftorres@gmail.com>
2022-11-22 18:59:15 -0300

    chore: first commit

---
+++ src/main/java/com/github/yulken/poc_kibana_traces/PocKibanaTracesApplication.java
@@ -0,0 +1,13 @@
+package com.github.yulken.poc_kibana_traces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocKibanaTracesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocKibanaTracesApplication.class, args);
+	}
+
+}
